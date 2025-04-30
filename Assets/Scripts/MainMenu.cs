dc4e46f5eeffef6235c37974cb5c434f88eefafa
zenxha <komays5042@gmail.com>
2025-04-30 13:48:04 -0700

    add buttons level 1,2,3

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
+    public void Level1()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene("Enemy Pathfinding");
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene("Enemy Pathfinding");
     }
     public void Quit()
     {
